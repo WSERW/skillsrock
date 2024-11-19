4135bdcf29328972777c0ff4327a528745351423
WSERW <sergeyYH@yandex.ru>
2024-11-19 14:51:37 +0300

    task2 fix

--- Task2/script.js
+++ Task2/script.js
@@ -8,15 +8,25 @@ const list = new Map()
 let id = 0
 
 const addTask = (name) => {
-    list.set(++id, { name: name, isDone: false })
+    let curId = ++id
+    list.set(curId, { name: name, isDone: false })
     const newElem = document.createElement('li')
     newElem.className = 'task'
     newElem.innerHTML = `
             <span class="task_name">${name}</span>
             <input type="checkbox" class="task_done">
-            <button class="task_delete"></button>`
+            <button class="task_delete">x</button>`
+    const delBtn = newElem.querySelector('.task_delete')
+    delBtn.addEventListener('click', function () {
+        list.delete(curId)
+        newElem.remove()
+    })
+    const doneBtn = newElem.querySelector('.task_done')
+    doneBtn.addEventListener('click', function () {
+        list.get(curId).isDone = !list.get(curId).isDone
+        newElem.classList.toggle('done')
+    })
     taskList.append(newElem)
-    console.log(list)
 }
 
 btnAdd.addEventListener('click', () => addTask(input.value))
